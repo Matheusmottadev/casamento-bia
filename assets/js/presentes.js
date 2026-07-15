9443c6ef9ffc57f949c3671f3113e8c19043e7c2
Matheusmottadev <Oimatheus64@gmail.com>
2026-07-14 23:25:28 -0300

    Atualiza preco do capacete para dez reais

--- assets/js/presentes.js
+++ assets/js/presentes.js
@@ -62,7 +62,7 @@ if (purchaseGrid && reservationGrid) {
       id: "helmet-against-rolling-pin",
       image: "./assets/images/gifts/purchase/helmet-against-rolling-pin-user.png",
       title: "Capacete contra rolo de macarrao",
-      price: "R$ 1,00",
+      price: "R$ 10,00",
     },
     {
       id: "broken-couple-fund",
